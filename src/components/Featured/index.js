17281cb117e18a2729dc9f8d5a578b4cd4abeae6
Tanner Pubins <t.pubins@icloud.com>
2023-11-13 21:50:04 -0700

    Fix title style for featured projects and open link in new tab.

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -69,7 +69,7 @@ const Featured = () => {
                 </div>
 
                 <StyledProjectImgWrapper>
-                  <a href={external || github || '#'}>
+                  <a href={external || github || '#'} target="_blank" rel="noopener noreferrer">
                     <div className="img-wrapper">
                       <div className="img-cont" />
                       <StyledProjectImage src={cover} alt={title} className="img" />
