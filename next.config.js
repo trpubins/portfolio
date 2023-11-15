2206d45b794ce00f0c28b19e9aff1012d0b3887d
Tanner Pubins <t.pubins@icloud.com>
2023-11-15 16:14:32 -0700

    Use newer versions of next and react.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+
+  images: {
+    unoptimized: true  // disable the Image Optimization API for static SPA export
+  },
 
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
