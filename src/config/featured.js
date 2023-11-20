e58d21660de3515e10963c80354628550d8c9c72
Tanner Pubins <t.pubins@icloud.com>
2023-11-20 12:13:02 -0700

    Improve featured project descriptions.

--- src/config/featured.js
+++ src/config/featured.js
@@ -6,7 +6,7 @@ module.exports = [
     github: 'https://github.com/topshelfsoftware/real-estate-community-ranking-backend',
     external: 'https://www.movingtophoenix.net/55-communities-ranking-tool',
     descriptionHtml:
-      'API-based software solution to rank 55+ communities in the Phoenix metro area based on a homebuyer&apos;s priorities. Takes needs/wants as input and returns a personalized list of the highest ranked communities.',
+      'API-based web app to rank 55+ communities in the Phoenix metro area based on a homebuyer&apos;s priorities. Takes needs/wants as input and returns a personalized list of the highest ranked communities.',
     techs: ['Python', 'JavaScript', 'AWS'],
   },
   {
@@ -14,7 +14,7 @@ module.exports = [
     cover: '/proj02.svg',
     github: 'https://github.com/trpubins/pokemon-shiny-hunting',
     descriptionHtml:
-      'Cross-platform application to automate the arduous shiny hunting process. Performs image recognition by leveraging basic functions of OpenCV. Runs on both macOS and Windows.',
+      'Cross-platform desktop application to automate the arduous shiny hunting process. Performs image recognition by leveraging basic functions of OpenCV. Runs on both macOS and Windows.',
     techs: ['Python', 'OpenCV', 'ASGI'],
   },
   {
@@ -22,7 +22,7 @@ module.exports = [
     cover: '/proj03.svg',
     github: 'https://github.com/trpubins/Gifter',
     descriptionHtml:
-      'Native iOS application that matches gifters in a secret santa gift exchange. Notifies each participant separately to keep gifter assignments a surprise.',
+      'Native iOS app that matches gifters in a secret santa gift exchange. Notifies each participant separately to keep gifter assignments a surprise.',
     techs: ['Swift', 'SwiftUI'],
   },
 ];
