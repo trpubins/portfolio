42f2790bc1b1cc5eb92f5cf0118ab7bd478f1074
Tanner Pubins <t.pubins@icloud.com>
2023-12-21 13:09:23 -0700

    Fix favicon issue by adding icons and removing manifest file.

--- src/layouts/base.js
+++ src/layouts/base.js
@@ -7,14 +7,12 @@ const BaseLayout = ({ children }) => {
     <div id="main">
       <Head>
         <title>Tanner Pubins | SW Architect</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#181818" />
-        <meta name="msapplication-TileColor" content="#ffffff" />
-        <meta name="theme-color" content="#ffffff" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#F8845D" />
+        <meta name="msapplication-TileColor" content="#161922" />
+        <meta name="theme-color" content="#161922" />
         <meta
           name="viewport"
           key="viewport"
