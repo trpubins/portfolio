85e7a6d7e3fccc89d7aa3362d3fab3eb9f6b8fee
Tanner Pubins <t.pubins@icloud.com>
2025-03-16 00:05:29 -0700

    Add hook for querying screen width.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,3 +1,4 @@
+export { default as useMediaQuery } from './useMediaQuery';
 export { default as useOnClickOutside } from './useOnClickOutside';
 export { default as useScrollDirection } from './useScrollDirection';
 export { default as useNearScreen } from './useNearScreen';
---
+++ src/hooks/useMediaQuery.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+const useMediaQuery = (maxWidth) => {
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const query = `(max-width: ${maxWidth})`;
+    const media = window.matchMedia(query);
+    if (media.matches !== matches) {
+      setMatches(media.matches);
+    }
+    const listener = () => setMatches(media.matches);
+    media.addEventListener('change', listener);
+    return () => media.removeEventListener('change', listener);
+  }, [matches, maxWidth]);
+
+  return matches;
+};
+
+export default useMediaQuery;
